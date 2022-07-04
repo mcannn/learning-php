7ca74761d8fa3f23a5216b660a6cd205d4806d24
mcannn <mcanorge@gmail.com>
2022-07-04 12:12:09 +0300

    first commit

---
+++ syntax/intro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$color="blue";
+echo "ana sayfa <br>";
+ECHO "ana sayfa1 <br>";
+echo "renk= $color <br>";
+
+//yorumsatiri1
+#yorumsatiri2
+/* yorum
+satiri3
+*/
+$txt = "txt variable";
+$x = 5 /* +10 */ +5;
+$y = 20;
+$z = 30;
+echo $x."<br>";
+echo $y."<br>";
+echo $z."<br>";
+echo "variable name= $txt"."<br>";
+echo "x= ".$x."<br>";
+echo "y= ".$y."<br>";
+echo "z= ".$z."<br>";
+echo "x + y = $x + $y = ". $x + $y."<br>";
+echo strlen($txt);
+?>
+
+</body>
+</html>
