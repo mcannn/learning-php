79b2a027dc5101a239eecb21380eef9a346171fc
mcannn <mcanorge@gmail.com>
2022-07-07 11:00:57 +0300

    OOP

---
+++ OOP/abstract_class.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//animals adında bir abstract class oluştur
+abstract class Animals{
+    public $isim;
+    public function __construct($isim){
+        $this->isim=$isim;
+    }
+    abstract public function Animal():string;
+}
+//cat adında bir child class oluştur
+class Cat extends Animals{
+    public function Animal() :string{
+        return "this is $this->isim - Kedi class!";
+    }//ekran çıktısı: this is CAT - Kedi class!
+}
+//dog adında bir child class oluştur
+class Dog extends Animals{
+    public function Animal() :string{
+        return "this is $this->isim - Köpek class!";
+    }//ekran çıktısı: this is DOG - Köpek class!
+}
+//fish adında bir child class oluştur
+class Fish extends Animals{
+    public function Animal() :string{
+        return "this is $this->isim - Balık class!";
+    }//ekran çıktısı: this is FISH - Balık class!
+}
+//child class üzerinden nesne üret
+$kedi =new Cat("CAT");
+echo $kedi->Animal()."<br>";
+$köpek = new Dog("DOG");
+echo $köpek ->Animal()."<br>";
+$balık =new Fish("FISH");
+echo $balık->Animal()."<br>";
+?> 
+</body>
+</html>
