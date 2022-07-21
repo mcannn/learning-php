940f3c7e126e8eb379f146ffb41eab6968dd97a1
mcannn <mcanorge@gmail.com>
2022-07-21 16:19:03 +0300

    form_example

--- forms/form_example.php
+++ forms/form_example.php
@@ -15,13 +15,20 @@
     Ad : <input type="text" name="ad"><br><br>
     Soyad : <input type="text" name="soyad"><br><br>
     Adres : <textarea type="text" name="adres" rows="3" cols="25"></textarea><br><br>
-
     <!--html radiobuton sınıf seçimi-->
-    Sınıf: <br>
-    1.Sınıf<input type="radio" name="sınıf" value="1"><br>
-    2.Sınıf<input type="radio" name="sınıf" value="2"><br>
-    3.Sınıf<input type="radio" name="sınıf" value="3"><br>
-    4.Sınıf<input type="radio" name="sınıf" value="4"><br><br>
+    Sınıf: <select name="sınıf">
+    <option value=""></option>
+    <option value="1.Sınıf">1.Sınıf</option>
+    <option value="2.Sınıf">2.Sınıf</option>    
+    <option value="3.Sınıf">3.Sınıf</option>    
+    <option value="4.Sınıf">4.Sınıf</option>  
+    </select><br>
+    Hobiler: <br>
+    <input type="checkbox" name="Hobiler[]" value="Sinema">Sinemaya Gitmek<br>
+    <input type="checkbox" name="Hobiler[]" value="Alıveriş">Alışverişe Çıkmak<br>
+    <input type="checkbox" name="Hobiler[]" value="Tiyatro">Tiyatroya Gitmek<br>
+    <input type="checkbox" name="Hobiler[]" value="Spor">Spor Yapmak<br>
+    <input type="checkbox" name="Hobiler[]" value="Video">Video Oyunları Oynamak<br><br>
 
     Cinsiyet:<br>
     Erkek
@@ -45,14 +52,20 @@
         $GelenSoyad = $_POST["soyad"];
         $GelenAdres = $_POST["adres"];
         $GelenSınıf = $_POST["sınıf"];
+        $GelenHobi = $_POST["Hobiler"];
         $GelenCinsiyet = $_POST["cinsiyet"];
+        
         //verileri görüntüle
         echo "---ALINAN VERİLER---<br>";
-        echo "Ad : " .$GelenAd. "<br>";
-        echo "Soyad : " .$GelenSoyad. "<br>";
-        echo "Adres : " .$GelenAdres. "<br>";
-        echo "Sınıf : " .$GelenSınıf. "<br>";
-        echo "Cinsiyet : " .$GelenCinsiyet. "<br>";
+        echo "<b>Ad : </b>" .$GelenAd. "<br>";
+        echo "<b>Soyad : </b>" .$GelenSoyad. "<br>";
+        echo "<b>Adres : </b>" .$GelenAdres. "<br>";
+        echo "<b>Sınıf : </b>" .$GelenSınıf. "<br>";
+        echo "<b>Hobiler : </b>";
+        foreach($GelenHobi as $Bilgiler){
+            echo $Bilgiler .", ";
+        }
+        echo "<br><b>Cinsiyet : </b>" .$GelenCinsiyet. "<br>";
         /*
         //Dizi şeklinde görüntüle
         $GelenVeriler = $_POST;
