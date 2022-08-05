ea2ce009be5c3af5f7fc428b80f9e85bee454d59
mcannn <mcanorge@gmail.com>
2022-08-05 16:50:17 +0300

    currency_info

--- apps/doviz_bilgisi.php
+++ apps/doviz_bilgisi.php
@@ -9,11 +9,11 @@
 <body>
     <?php
     $link = simplexml_load_file("https://www.tcmb.gov.tr/kurlar/today.xml");
-    
+    /*
     echo "<pre>";
     print_r($link);
     echo "</pre>";
-    /*
+    */
     foreach($link->Currency as $key=>$value){
         echo "<p><b>PARA BİRİMİ(TR) : </b>".$value->Isim."<br>";
         echo "<b>PARA BİRİMİ(EN) : </b>".$value->CurrencyName."<br>";
@@ -21,7 +21,7 @@
         echo "<b>SATIŞ : </b>".$value->ForexSelling."<br>";
         echo "<b>EFEKTİF ALIŞ : </b>".$value->BanknoteBuying."<br>";
         echo "<b>EFEKTİF SATIŞ : </b>".$value->BanknoteSelling."<br><br></p>";
-    }    */
+    }
     ?>
 </body>
 </html>
