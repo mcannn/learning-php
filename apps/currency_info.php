ade043027ae6c4f8e79ba448a9c89297b068bf1b
mcannn <mcanorge@gmail.com>
2022-08-08 10:44:29 +0300

    currency_info

--- apps/currency_info.php
+++ apps/currency_info.php
@@ -14,6 +14,24 @@
     print_r($link);
     echo "</pre>";
     */
+    try{
+        $veritabaniBaglantisi = new PDO("mysql:host=localhost;dbname=test;charset=UTF8","root", "");
+    }catch(PDOException $hataMesaji){
+        echo "bağlantı hatası!! <br>";
+        echo "hata açıklaması: ".$hataMesaji->getMessage();
+        die();
+    }
+    //veritabanında currency adında tablo yoksa yenisini oluştur
+    $tablo = "CREATE TABLE IF NOT EXISTS currency(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        birim_tr VARCHAR(50) NOT NULL,
+        birim_en VARCHAR(50) NOT NULL,
+        alis FLOAT(10),
+        satis FLOAT(10),
+        efektif_alis FLOAT(10),
+        efektif_satis FLOAT(10))";
+    $veritabaniBaglantisi->exec($tablo);
+    //verileri ekrana yazdır, veritabanına ekle
     foreach($link->Currency as $key=>$value){
         echo "<p><b>PARA BİRİMİ(TR) : </b>".$value->Isim."<br>";
         echo "<b>PARA BİRİMİ(EN) : </b>".$value->CurrencyName."<br>";
@@ -21,7 +39,11 @@
         echo "<b>SATIŞ : </b>".$value->ForexSelling."<br>";
         echo "<b>EFEKTİF ALIŞ : </b>".$value->BanknoteBuying."<br>";
         echo "<b>EFEKTİF SATIŞ : </b>".$value->BanknoteSelling."<br><br></p>";
+        //veritabanına ekle
+        $ekle = $veritabaniBaglantisi->exec("INSERT INTO currency(id, birim_tr, birim_en, alis, satis, efektif_alis, efektif_satis) 
+        VALUES ('$value', '$value->Isim', '$value->CurrencyName', '$value->ForexBuying', '$value->ForexSelling', '$value->BanknoteBuying', '$value->BanknoteSelling')");
     }
+    $veritabaniBaglantisi = NULL;
     ?>
 </body>
 </html>
