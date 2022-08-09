82defbecd43a9587deab2faebadfa8fce9d3982f
mcannn <mcanorge@gmail.com>
2022-08-09 11:14:16 +0300

    banner

---
+++ apps/banner.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    try{
+        $veritabaniBaglantisi = new PDO("mysql:host=localhost;dbname=test;charset=UTF8","root","");
+    }catch(PDOException $hata){
+        echo "bağlantıda hata oluştu!<br/>";
+        echo "hata açıklaması: ". $hata -> getMessage();
+        die();
+    }
+    ?>
+<table width="1000" align="center" border="0" cellpadding="0" cellspacing="0">
+    <tr height="200">
+        <td align="center"><img src="../../Resimler/banner1.jpg" border="0"></td>
+    </tr>
+</table> 
+    <?php
+    $veritabaniBaglantisi = NULL;
+    ?>
+</body>
+</html>
